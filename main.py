87306f9206dca7f3bc9da83cf458345e200963fc
kayleehou <kayleehou@yahoo.com>
2023-02-28 13:52:31 -0800

    x

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ from flask import render_template  # import render_template from "public" flask 
 
 # import "packages" from "this" project
 from __init__ import app  # Definitions initialization
-from model.jokes import initJokes
+# from model.jokes import initJokes
 from model.users import initUsers
 
 # setup APIs
-from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
+# from api.covid import covid_api # Blueprint import api definition
+# from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
 
 # register URIs
-app.register_blueprint(joke_api) # register api routes
-app.register_blueprint(covid_api) # register api routes
+# app.register_blueprint(joke_api) # register api routes
+# app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(app_projects) # register app pages
 
@@ -121,7 +121,7 @@ def twenty():
 
 @app.before_first_request
 def activate_job():
-    initJokes()
+    # initJokes()
     initUsers()
 
 # this runs the application on the development server
