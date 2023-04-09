281a5950a9800e6721835a0cebbe532cc09a8c28
kayleehou <kayleehou@yahoo.com>
2023-04-09 16:03:01 -0700

    a

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ if __name__ == "__main__":
     # change name for testing 
     from flask_cors import CORS
     cors = CORS(app)
-    app.run(debug=True, host="0.0.0.0", port="8338")
+    app.run(debug=True, host="0.0.0.0", port="8332")
 
 
     
